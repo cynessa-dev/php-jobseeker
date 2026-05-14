739f636602c07cf2bc0fc5e07b389402a0a89d62
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-14 22:19:52 +0800

    ✨ feat: add routes.php for routing

---
+++ app/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @var mixed $router */
+$router->get('/', 'controllers/home.php');
+$router->get('/listings', 'controllers/listings/index.php');
+$router->get('/listings/create', 'controllers/listings/create.php');
+
+?>
