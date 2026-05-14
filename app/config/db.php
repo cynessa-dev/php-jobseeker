95766f720aea229d9420be9aba060738a5b9e844
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-14 20:40:01 +0800

    ✨ feat: add db config with env variables for database security

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => $_ENV['MYSQL_HOST'],
+    'port' => $_ENV['MYSQL_PORT'],
+    'dbname' => $_ENV['MYSQL_DBNAME'],
+    'username' => $_ENV['MYSQL_USERNAME'],
+    'password' => $_ENV['MYSQL_PASSWORD']
+];
+
+?>
