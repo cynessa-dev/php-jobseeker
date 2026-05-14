171e7b7801f7f56fafffbad3e03971266f26e33b
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-14 22:20:12 +0800

    ✨ feat: configure routing and db for root page

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,18 @@
 <?php
 
-require "../helpers.php";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-loadView("home");
+require "../helpers.php";
+require basePath('Router.php');
+require basePath('Database.php');
+
+$router = new Router();
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
 
 ?>
