99eff93e31ea6c6c321bbb295467298d5dc565cc
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-16 23:18:57 +0800

    ✨ feat: modify home to display data from db

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -15,23 +15,27 @@
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <?php foreach (isset($listings) ? $listings : [] as $listing): ?>
-                <!-- Job Listing 1: Software Engineer -->
+                <!-- Listings -->
                 <div class="rounded-lg shadow-md bg-white">
                     <div class="p-4">
-                        <h2 class="text-xl font-semibold">Software Engineer</h2>
+                        <h2 class="text-xl font-semibold"><?= htmlspecialchars($listing->title) ?></h2>
                         <p class="text-gray-700 text-lg mt-2">
-                            We are seeking a skilled software engineer to develop
-                            high-quality software solutions.
+                            <?= htmlspecialchars($listing->description) ?>
                         </p>
                         <ul class="my-4 bg-gray-100 p-4 rounded">
-                            <li class="mb-2"><strong>Salary:</strong> $80,000</li>
+                            <li class="mb-2"><strong>Salary: </strong><?= htmlspecialchars(formatSalary($listing->salary)) ?></li>
                             <li class="mb-2">
-                                <strong>Location:</strong> New York
+                                <strong>Location:</strong> <?= htmlspecialchars($listing->city) ?>
                                 <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span>
                             </li>
                             <li class="mb-2">
-                                <strong>Tags:</strong> <span>Development</span>,
-                                <span>Coding</span>
+                                <strong>Tags:</strong>
+                                <?php
+                                $tags = explode(',', $listing->tags);
+                                foreach ($tags as $tag) {
+                                    echo '<span class="text-xs rounded-full px-2 py-1 mr-1">' . htmlspecialchars(trim($tag)) . '</span>';
+                                }
+                                ?>
                             </li>
                         </ul>
                         <a href="details"
