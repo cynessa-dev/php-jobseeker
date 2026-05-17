0f58cb3f8c90d659704a26ae327364d6d27ba651
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-17 09:03:13 +0800

    ✨ feat: add post for register and login

--- app/routes.php
+++ app/routes.php
@@ -6,5 +6,7 @@ $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
 $router->get('/auth/register', 'controllers/auth/register.php');
 $router->get('/auth/login', 'controllers/auth/login.php');
+$router->post('/auth/register', 'controllers/auth/register.php');
+$router->post('/auth/login', 'controllers/auth/login.php');
 
 ?>
