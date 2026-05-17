33e57d46ce550c7bec3801477cffbc31325a33f7
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-17 09:46:43 +0800

    ✨ feat: add parameterized route for listing

--- app/routes.php
+++ app/routes.php
@@ -9,5 +9,6 @@ $router->get('/auth/login', 'controllers/auth/login.php');
 $router->post('/auth/register', 'controllers/auth/register.php');
 $router->post('/auth/login', 'controllers/auth/login.php');
 $router->post('/listings/create', 'controllers/listings/create.php');
+$router->get('/listings/{id}', 'controllers/listings/show.php');
 
 ?>
