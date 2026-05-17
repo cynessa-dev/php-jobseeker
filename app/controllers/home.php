f60e64eb684a8e3e5b1ad226c9e9b1a590a81356
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-05-17 10:23:43 +0800

    ✨ feat: change query to get last 3 entries in table listings for true recent

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,8 +3,8 @@
 $config = require basePath('config/db.php');
 $db = new Database($config);
 
-// Fetch all listings from the database for display on the home page
-$listings = $db->query("SELECT * FROM listings")->fetchAll();
+// Fetch latest 3 listings from the database for display on the home page
+$listings = $db->query("SELECT * FROM listings ORDER BY id DESC LIMIT 3")->fetchAll();
 
 // Demo purpose only -> Adds dummy data for testing
 if (count($listings) === 0) {
